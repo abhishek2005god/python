5952875613e0ec3bcb2afa1a694c021cd73a2551
amrakh7 <abhishekrakh6@gmail.com>
2026-06-13 21:41:50 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World");
+
+Monday = 20
+Tuesday = 30
+Avg = (Monday +Tuesday/2);
+
+
+age=int(input())
+if (age>=18):
+    test = input()
+    if test == "pass":
+          print("Eligible for driving license")
+    else : 
+          print("Not Eligible For driving license")
+else :
+    print("Not Eligible For driving license")
